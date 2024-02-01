8a4a1727e3e9537e6f8dc9355ea4fa8d09c230a1
dawidek14472 <dawid.okojew@o2.pl>
2024-02-01 16:21:43 +0100

    added function that adds a photo in javascript

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ const app = document.querySelector('.appText')
 const wrapper = document.querySelector('.wrapper')
 const photoMode = document.querySelector('.photoMode')
 const photoIcon = document.querySelector('.fa-image')
+const divAppPhoto = document.querySelector('.appPhoto')
 
 const textOne = 'Pies na tym zdjęciu jest spokojny, opanowany'
 const textTwo =
@@ -16,11 +17,17 @@ const textFour = 'Pies jest zrelaksowany, prawdopodobnie lekko zmęczony po spac
 const swapSection = () => {
 	wrapper.classList.add('none')
 	photoMode.classList.remove('none')
+	photoMode.classList.remove('none')
+	divAppPhoto.classList.remove('imgGalery')
+	divAppPhoto.classList.remove(photo)
+	app.textContent = ''
 }
 
-const chooseText = text => {
+const chooseText = (text, photo) => {
 	wrapper.classList.remove('none')
 	photoMode.classList.add('none')
+	divAppPhoto.classList.add('imgGalery')
+	divAppPhoto.classList.add(photo)
 	app.textContent = ''
 	for (let i = 0; i < text.length; i++) {
 		setTimeout(() => {
@@ -29,8 +36,8 @@ const chooseText = text => {
 	}
 }
 
-btn1.addEventListener('click', () => chooseText(textOne))
-btn2.addEventListener('click', () => chooseText(textTwo))
-btn3.addEventListener('click', () => chooseText(textThree))
-btn4.addEventListener('click', () => chooseText(textFour))
+btn1.addEventListener('click', () => chooseText(textOne, 'imgOne'))
+btn2.addEventListener('click', () => chooseText(textTwo, 'imgTwo'))
+btn3.addEventListener('click', () => chooseText(textThree, 'imgThree'))
+btn4.addEventListener('click', () => chooseText(textFour, 'ImgFour'))
 photoIcon.addEventListener('click', () => swapSection())
